b111335855b3e2630bf60b920c7d9d3df30a32f8
syyeye7sush <malikzEEshanshaniff@gmail.com>
2026-05-08 09:35:41 +0500

    hardcode cookies

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import yt_dlp
 import os
 import time
+import tempfile
 
 app = Flask(__name__)
 CORS(app)
@@ -10,13 +11,33 @@ CORS(app)
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
-COOKIES_FILE = os.path.join(os.path.dirname(__file__), 'cookies.txt')
+COOKIES_CONTENT = """# Netscape HTTP Cookie File
+.youtube.com\tTRUE\t/\tFALSE\t1841201179\tSID\tg.a0009ghT5Ox0JcaL6KX1qJWADHbU1tDfd7Ujylj6pFw9pTfcpNSDU7vQLskdJREebAPgtIpb1QACgYKAcoSARASFQHGX2Mi3a-ROwijvC-c-mwNiA3KlxoVAUF8yKofD5AgMa3AQpjuahBdeh8S0076
+.youtube.com\tTRUE\t/\tTRUE\t1841201179\tSAPISID\t7Hb23n_godvp3H8l/Aet7qcP8VprT44Yer
+.youtube.com\tTRUE\t/\tTRUE\t1841201179\tSSID\tAu01dIK7ZvVeETMSc
+.youtube.com\tTRUE\t/\tFALSE\t1841201179\tAPISID\ti7X_Kwea_Anhbw0B/AmYZ2D03KFnnJmPId
+.youtube.com\tTRUE\t/\tFALSE\t1841201179\tHSID\tA6UDhq3UN8ftuPvFP
+.youtube.com\tTRUE\t/\tTRUE\t1841201179\t__Secure-1PSID\tg.a0009ghT5Ox0JcaL6KX1qJWADHbU1tDfd7Ujylj6pFw9pTfcpNSDOIkcdNcOV9xEz7EyhlH57gACgYKAWgSARASFQHGX2MiSKyDVQiVaXWdFykRzZByShoVAUF8yKqMe5e7l7dMc4oGnDggCUDK0076
+.youtube.com\tTRUE\t/\tTRUE\t1841201179\t__Secure-3PSID\tg.a0009ghT5Ox0JcaL6KX1qJWADHbU1tDfd7Ujylj6pFw9pTfcpNSDQk0-14mLMQcKiIIOYc02OAACgYKAaUSARASFQHGX2MiRqrcIQOMYcoCP834s8jgSxoVAUF8yKqOuwDxXLImsUcdZ1StuVTF0076
+.youtube.com\tTRUE\t/\tFALSE\t1809665185\tSIDCC\tAKEyXzWIuc53UuK3pBICHy5kVm4NMJttjO-tQTkgXeuM1dXD2XAV58YUV7oLD5O9GEf6TTQN
+.youtube.com\tTRUE\t/\tTRUE\t1809665185\t__Secure-1PSIDCC\tAKEyXzVNH6QFz5AqRarWFQasoahskw6KsdJpBcEfwrYkigdZ5z4D4Sw7ri1B2JzHT__Ymhxj
+.youtube.com\tTRUE\t/\tTRUE\t1809665185\t__Secure-3PSIDCC\tAKEyXzU-BHd_UHmy24B_cFZPxyBqPm8enyd9qIE4M_gFLTR_rx95SX-4fvFDbVs-RE9aDQq2
+.youtube.com\tTRUE\t/\tTRUE\t1793681180\tVISITOR_INFO1_LIVE\tRQiF6Mw9GnQ
+.youtube.com\tTRUE\t/\tTRUE\t1841201181\tPREF\ttz=Asia.Kolkata
+"""
+
+def get_cookies_file():
+    tmp = tempfile.NamedTemporaryFile(mode='w', suffix='.txt', delete=False)
+    tmp.write(COOKIES_CONTENT)
+    tmp.close()
+    return tmp.name
 
 def get_ydl_opts(label, output_template):
+    cookies_file = get_cookies_file()
     base = {
         'quiet': True,
         'no_warnings': True,
-        'cookiefile': COOKIES_FILE,
+        'cookiefile': cookies_file,
         'outtmpl': output_template,
         'merge_output_format': 'mp4',
         'extractor_args': {'youtube': {'player_client': ['android']}},
@@ -56,10 +77,11 @@ def analyze():
     if not url:
         return jsonify({'error': 'URL required'}), 400
     try:
+        cookies_file = get_cookies_file()
         opts = {
             'quiet': True,
             'no_warnings': True,
-            'cookiefile': COOKIES_FILE,
+            'cookiefile': cookies_file,
             'extractor_args': {'youtube': {'player_client': ['android']}},
             'http_headers': {'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 Chrome/90.0.4430.91 Mobile Safari/537.36'},
         }
