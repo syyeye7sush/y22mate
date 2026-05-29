f33cf8f7f03f4c1253151fb8f8fb1feea0300b68
syyeye7sush <malikzEEshanshaniff@gmail.com>
2026-05-29 08:35:51 +0500

    syntax fix

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def download():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False
+    app.run(host='0.0.0.0', port=5000, debug=False)
